309050f2e6a43032f869df263f0daba4b1e186ce
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-11-15 00:46:42 -0400

    Display menu into Main class

--- src/com/nicoselominbooking/Main.java
+++ src/com/nicoselominbooking/Main.java
@@ -2,6 +2,15 @@ package com.nicoselominbooking;
 
 public class Main {
     static void main(String[] args) {
-
+        System.out.println("""
+                \n
+                1️⃣ - Book Car
+                2️⃣ - View All User Booked Cars
+                3️⃣ - View All Bookings
+                4️⃣ - View Available Cars
+                5️⃣ - View Available Electric Cars
+                6️⃣ - View all users
+                7️⃣ - Exit
+                """);
     }
 }
