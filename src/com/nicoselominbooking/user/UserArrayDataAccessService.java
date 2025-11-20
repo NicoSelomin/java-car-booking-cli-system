5dc7cb6bdf1474702ca9ea6c54d29aa8fb9ca151
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-11-20 17:02:29 -0400

    refactor: Extract UserDao interface and implement Array service

--- src/com/nicoselominbooking/user/UserArrayDataAccessService.java
+++ src/com/nicoselominbooking/user/UserArrayDataAccessService.java
@@ -2,15 +2,17 @@ package com.nicoselominbooking.user;
 
 import java.util.UUID;
 
-public class UserDAO {
-    private final static User[] USERS = {
+public class UserArrayDataAccessService implements UserDao{
+    private final static User[] users = {
             new User(UUID.randomUUID(), "Alice Smith"),
             new User(UUID.randomUUID(), "Bob Johnson"),
-            new User(UUID.randomUUID(), "Charlie Brown")
+            new User(UUID.randomUUID(), "Charlie Brown"),
+            new User(UUID.randomUUID(), "Jean Barron"),
     };
 
+    @Override
     public User[] getUsers(){
-        return USERS;
+        return users;
     }
 
 
---
+++ src/com/nicoselominbooking/user/UserDao.java
@@ -0,0 +1,8 @@
+package com.nicoselominbooking.user;
+
+
+import java.util.UUID;
+
+public interface UserDao {
+    User[] getUsers();
+}
